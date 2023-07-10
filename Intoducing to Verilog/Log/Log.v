b3ab28bd6ff48400a26fe707ac4e90bb8aab1c5d
Oleg Berezin <olegberezin2002@gmail.com>
2023-07-10 13:22:06 +0300

    new code fot lag task

--- Intoducing to Verilog/Log/Log.v
+++ Intoducing to Verilog/Log/Log.v
@@ -1,23 +1,17 @@
 module log(
     input [7:0]num,
 
-    output reg [3:0]log_num
+    output wire [3:0]log_num
 );
 
-always @(num) begin
-    case (num)
-        8'b00000001 : log_num = 0;
-        8'b00000010 : log_num = 1;
-        8'b00000100 : log_num = 2;
-        8'b00001000 : log_num = 3;
-        8'b00010000 : log_num = 4;
-        8'b00100000 : log_num = 5;
-        8'b01000000 : log_num = 6;
-        8'b10000000 : log_num = 7;
-        
-        default    : log_num = -1;
-    endcase 
-end
-
+assign log_num = num == 8'b00000001 ? 0 :
+                 num == 8'b00000010 ? 1 :
+                 num == 8'b00000100 ? 2 :
+                 num == 8'b00001000 ? 3 :
+                 num == 8'b00010000 ? 4 :
+                 num == 8'b00100000 ? 5 :
+                 num == 8'b01000000 ? 6 :
+                 num == 8'b10000000 ? 7 :
+                 -1;
+                    
 endmodule
-
