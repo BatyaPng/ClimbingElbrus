87cb440a9930d5961b7d4345c281eeb439834391
Oleg <olegberezin2002@gmail.com>
2023-07-19 23:01:06 +0400

    done pulse generator task

---
+++ 2 Week/PulseGenerator/PlsGen.v
@@ -0,0 +1,22 @@
+module pls_gen_n
+#(
+    parameter period = 2
+)
+(
+    input clk, reset,
+
+    output impulse
+);
+
+reg [period - 1]counter;
+
+always @(posedge clk) begin
+    if (reset)
+        counter <= 0;
+    else
+        counter <= (counter < period - 1) ? counter + 1 : 0;
+end
+
+assign impulse = (counter == period - 1) ? 1 : 0;
+
+endmodule
