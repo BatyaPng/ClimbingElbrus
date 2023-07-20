4ae89bc9f2c57c0f18decf0f9cfeb72acf0d3a5f
Oleg Berezin <olegberezin2002@gmail.com>
2023-07-21 01:29:15 +0300

    release FIFO task done

--- 2 Week/FIFO/FIFO.v
+++ 2 Week/FIFO/FIFO.v
@@ -17,20 +17,14 @@ reg [DATA_WIDTH] queue [FIFO_DEPTH];
 reg [CAPACITY] counter;
 
 always @(posedge clk) begin
-    $display("rd_val   - %d; wr_ready - %d", rd_val, wr_ready);
-    $display("rd_en    - %d; wr_en    - %d", rd_en, wr_en);
-    $display("counter %d", counter);
     if (reset) begin
-        counter <= 0;
-        $display("I'm here 22");    
+        counter <= 0; 
     end
     else if (rd_en & rd_val) begin
-        $display("Out[%d] %d", counter - 1, queue[counter - 1],);
         rd_data <= queue[counter - 1];
         counter <= counter - 1; 
     end
     else if (wr_en & wr_ready) begin
-        $display("In[%d] %d", counter, wr_data);
         queue[counter] <= wr_data;
         counter <= counter + 1;
     end
