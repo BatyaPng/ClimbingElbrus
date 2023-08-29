80cfe1150cc88188f1f41d98653215ea7527e8e6
Oleg <olegberezin2002@gmail.com>
2023-08-30 01:40:16 +0400

    refactoring

--- FPMultiplier/FPMultiplier.v
+++ FPMultiplier/FPMultiplier.v
@@ -1,5 +1,5 @@
 module fp_multiplier (
-    input clk, en,
+    input clk, en, reset,
     input [31:0] num1, num2,
 
     output reg [31:0] res,
@@ -10,21 +10,21 @@ wire sign1, sign2;
 assign sign1 = num1[31];
 assign sign2 = num2[31];
 
-wire [8:0] exp1, exp2;
+wire [7:0] exp1, exp2;
 assign exp1 = num1[30:23];
 assign exp2 = num2[30:23];
 
-wire [22:0] mant1, mant2;
-assign mant1 = num1[22:0];
-assign mant2 = num2[22:0];
+wire [24:0] mant1, mant2;
+assign mant1 = {1'b1, num1[22:0]};
+assign mant2 = {1'b1, num2[22:0]};
 
-wire [45:0] mant_mul_res;
+wire [47:0] mant_mul;
 wire man_val;
 wire overflow;
-multiplier #(23) mant_mul  (.clk(clk), .en(en),
-                    .op1(mant1), .op2(mant2),
-                    .res(mant_mul_res),
-                    .val(man_val), .overflow(overflow)    
+multiplier #(24) mant_mul (.clk(clk), .en(en),
+                           .op1(mant1), .op2(mant2),
+                           .res(mant_mul),
+                           .val(man_val), .overflow(overflow)    
 );
 
 endmodule
---
+++ FPMultiplier/Normalization/Normalization.v
@@ -0,0 +1,8 @@
+module normalization (
+    input  [48:0] mant_mul,
+    output [48:0] mant_norm
+);
+    
+
+    
+endmodule
